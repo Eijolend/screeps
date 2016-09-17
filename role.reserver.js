aae0e69eab6d6f8c173720358a0a24ea6bf59f2f
Eijolend <j.wallnoefer@hotmail.com>
2016-09-17 22:27:40 +0200

    debug

--- role.reserver.js
+++ role.reserver.js
@@ -3,7 +3,7 @@ module.exports = {
 		var closeto = creep.pos.findInRange(FIND_FLAGS,4,{filter: (f) => f.name == creep.memory.myflag});
 		if(closeto.length){
 			var con = Game.flags[creep.memory.myflag].pos.lookFor(FIND_STRUCTURES,{filter : (s) => s.structureType == STRUCTURE_CONTROLLER})[0];
-			console.log(con);
+			console.log(Game.flags[creep.memory.myflag].pos.lookFor(FIND_STRUCTURES,{filter : (s) => s.structureType == STRUCTURE_CONTROLLER}));
 			if (creep.reserveController(con) == ERR_NOT_IN_RANGE){
 				creep.moveTo(con);
 			}
