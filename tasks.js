8defed238031ef0b098996bf9cb3899c4a58ba88
Eijolend <j.wallnoefer@hotmail.com>
2016-09-17 14:11:08 +0200

    put all the rework to obtaining energy into a new task

--- tasks.js
+++ tasks.js
@@ -29,6 +29,28 @@ module.exports = {
 		}
 	},
 	
+	getenergy : function(creep,mysource){ //get energy in priority: dropped, container, storage, harvest
+		var mycontainer = mysource.pos.findInRange(FIND_STRUCTURES,3,{filter : (s) => s.structureType == STRUCTURE_CONTAINER})[0];
+		var stock = creep.room.find(FIND_STRUCTURES,{filter : (s) => s.structureType == STRUCTURE_STORAGE && s.store.energy > 0});
+		
+		var targets = mysource.pos.findInRange(FIND_DROPPED_ENERGY,3);
+		if (mycontainer != undefined && mycontainer.store.energy == mycontainer.storeCapacity){ //fixes container overflowing
+			get(creep,mycontainer);
+		}
+		else if (targets.length){
+			pick(creep,targets[0]);
+		}
+		else if(mycontainer != undefined && mycontainer.store.energy >= creep.carryCapacity){
+			get(creep,mycontainer);
+		}
+		else if(stock.length){
+			get(creep,stock[0]);
+		}
+		else if(creep.getActiveBodyparts(WORK)){
+			mine(creep,mysource);
+		}
+	},
+	
 	fill : function(creep,prioritylist){
 		for(i=0; i<prioritylist.length; i++){
 		    if(prioritylist[i]==STRUCTURE_CONTAINER || prioritylist[i]==STRUCTURE_STORAGE){
