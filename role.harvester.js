922ee5d5b9477335abc26f9834b17ef8e55c0d78
Eijolend <j.wallnoefer@hotmail.com>
2016-09-17 17:26:41 +0200

    rewrote role.harvester for its new purpose

--- role.harvester.js
+++ role.harvester.js
@@ -1,10 +1,6 @@
 /*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.harvester');
- * mod.thing == 'a thing'; // true
+ * GOAL: harvester should go to their flag and harves there.
+ * 			they also should build and maintain their own road to their destination.
  */
 
 var tasks = require('tasks');
@@ -19,35 +15,26 @@ module.exports = {
 	        creep.memory.delivering = true;
 	        creep.say('delivering');
 	    }
-	    if(creep.memory.delivering){
-			var containers = creep.room.find(FIND_STRUCTURES, {
-				filter : (structure) => (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity
-	    	});
-			var towers = creep.room.find(FIND_STRUCTURES, { filter : (structure) => structure.structureTYPE == STRUCTURE_TOWER && structure.energy < structure.energyCapacity});
-			if(containers.length){
-				if(creep.transfer(containers[0],RESOURCE_ENERGY)==ERR_NOT_IN_RANGE){
-			        creep.moveTo(containers[0]);
-			    }
+		
+		if(creep.room.name != creep.memory.home){
+			if(!creep.pos.findInRange(FIND_STRUCTURES,0,{filter: (s) => s.structureType == STRUCTURE_ROAD}).length){
+				creep.pos.createConstructionSite(STRUCTURE_ROAD);
 			}
-			else if(towers.length){
-				if(creep.transfer(towers[0],RESOURCE_ENERGY)==ERR_NOT_IN_RANGE){
-			        creep.moveTo(towers[0]);
+			if(creep.memory.delivering){
+				var targets = creep.room.find(FIND_CONSTRUCTION_SITES,{filter : (s) => s.structureType == STRUCTURE_ROAD);
+				if (targets.length){
+					tasks.construct(creep,targets[0]);
+				}
+				else{
+					creep.moveTo(Game.spawns['Spawn1]);
 				}
 			}
-			else { // build stuff if everything is full
-				var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-				if(targets.length){
-					if(creep.build(targets[0])==ERR_NOT_IN_RANGE){
-						creep.moveTo(targets[0]);
-					}
-				}
+			else{
+				tasks.mine(Game.flags[creep.memory.myflag].pos.lookFor(LOOK_SOURCES)[0]);
 			}
 		}
-		else{
-			var sources = creep.room.find(FIND_SOURCES);
-    		if(creep.harvest(sources[0])==ERR_NOT_IN_RANGE){
-    		        creep.moveTo(sources[0]);
-    		}
+		else if (creep.room.name != creep.memory.home){
+			tasks.fill(creep,[STRUCTURE_STORAGE]);
 		}
 	}
 
