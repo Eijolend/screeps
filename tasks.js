a3bc0d74372d0350d6c0cd996f3e9d09bcb11c40
Eijolend <j.wallnoefer@hotmail.com>
2016-09-17 14:25:13 +0200

    Revert "fixes"
    This reverts commit 53fb2a707557cc01e3c815aba58d6a89ed63d5a7.

--- tasks.js
+++ tasks.js
@@ -35,7 +35,7 @@ module.exports = {
 		
 		var targets = mysource.pos.findInRange(FIND_DROPPED_ENERGY,3);
 		if (mycontainer != undefined && mycontainer.store.energy == mycontainer.storeCapacity){ //fixes container overflowing
-			tasks.get(creep,mycontainer);
+			get(creep,mycontainer);
 		}
 		else if (targets.length){
 			pick(creep,targets[0]);
