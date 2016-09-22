79c12c6b0a1696b637a087a1312f3a39c5e0aecd
Eijolend <j.wallnoefer@hotmail.com>
2016-09-23 00:05:25 +0200

    finished formulating the remote upgrader role

--- remote.upgrader.js
+++ remote.upgrader.js
@@ -17,15 +17,24 @@ module.exports = {
 	    }
 		if(creep.memory.upgrading){
 			if(creep.room.name == Game.flags[creep.memory.myflag].pos.roomName){
-				//upgrade
+				if(creep.upgradeController(creep.room.controller)==ERR_NOT_IN_RANGE){
+					creep.moveTo(creep.room.controller);
+				}
 			}
 			else{
-				//moveTo myflag
+				creep.moveTo(Game.flags[creep.memory.myflag]);
 			}
 		}
 		else{
 			if(creep.room.name == Game.flags[creep.memory.myflag].pos.roomName){
-				//change to builder and place down a spawn if there is no spawn, else change to normal upgrader in new room
+				var spawns = creep.room.find(FIND_MY_STRUCTURES, {filter : (s) => s.structureType == STRUCTURE_SPAWN});
+				if (spawns.length){
+					creep.memory.role = 'upgrader';
+				}
+				else{
+					creep.memory.role = 'builder';
+					Game.flags['placespawn'].pos.createConstructionSite(STRUCTURE_SPAWN);
+				}
 			}
 			else{
 				var sources = creep.room.find(FIND_SOURCES);
