194847b73f69c7388734a5629c344690bc49f0b1
Julius Wallnöfer <j.wallnoefer@hotmail.com>
2016-09-22 15:51:22 +0200

    now uses taks.getenergy(); did I not do this before?

--- role.upgrader.js
+++ role.upgrader.js
@@ -29,25 +29,7 @@ module.exports = {
          else { //get energy in priority: dropped, container, storage, harvest
 			var sources = creep.room.find(FIND_SOURCES);
 			var mysource = sources[1];
-			var mycontainer = mysource.pos.findInRange(FIND_STRUCTURES,3,{filter : (s) => s.structureType == STRUCTURE_CONTAINER})[0];
-			var stock = creep.room.find(FIND_STRUCTURES,{filter : (s) => s.structureType == STRUCTURE_STORAGE && s.store.energy > 0});
-			
-			var targets = mysource.pos.findInRange(FIND_DROPPED_ENERGY,3);
-			if (mycontainer != undefined && mycontainer.store.energy == mycontainer.storeCapacity){ //fixes container overflowing
-				tasks.get(creep,mycontainer);
-			}
-			else if (targets.length){
-				tasks.pick(creep,targets[0]);
-			}
-			else if(mycontainer != undefined && mycontainer.store.energy >= creep.carryCapacity){
-				tasks.get(creep,mycontainer);
-			}
-            else if(stock.length){
-                tasks.get(creep,stock[0])
-    		}
-			else{
-				tasks.mine(creep,mysource)
-			}
+			task.getenergy(creep,mysource);
 	    }
     }
 };
