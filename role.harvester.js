0f7aa0857219c0adcf1249d6fe615812da017e61
Eijolend <j.wallnoefer@hotmail.com>
2016-10-07 20:20:37 +0200

    bugfix

--- role.harvester.js
+++ role.harvester.js
@@ -30,7 +30,7 @@ module.exports = {
 					creep.repair(roadatpoint[0]);
 				}
 				else{
-					creep.moveTo(Game.rooms[creep.memory.homeRoom].find(FIND_STRUCTURES,{filter: (s) => s.structureType == STRUCTURE_STORAGE });
+					creep.moveTo(Game.rooms[creep.memory.homeRoom].find(FIND_STRUCTURES,{filter: (s) => s.structureType == STRUCTURE_STORAGE }) );
 				}
 			}
 			else{
