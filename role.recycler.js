575382f3ea4a1ac06912249fb635ade44d4eba1c
Eijolend <j.wallnoefer@hotmail.com>
2016-10-12 19:55:28 +0200

    recyclers now work from remote rooms if a homeRoom is set

--- role.recycler.js
+++ role.recycler.js
@@ -2,8 +2,13 @@ var tasks = require('tasks');
 
 module.exports = {
 	run : function(creep){
-		var spawn = creep.room.find(FIND_STRUCTURES,
-			{filter : (s) => s.structureType == STRUCTURE_SPAWN})[0];
-		tasks.recycle(creep,spawn);
+		if(creep.memory.homeRoom == undefined || creep.room.name == creep.memory.homeRoom ){
+			var spawn = creep.room.find(FIND_STRUCTURES,
+				{filter : (s) => s.structureType == STRUCTURE_SPAWN})[0];
+			tasks.recycle(creep,spawn);	
+		}
+		else{
+			creep.moveTo(Game.rooms[creep.memory.homeRoom].find(FIND_STRUCTURES,{filter: (s) => s.structureType == STRUCTURE_SPAWN })[0]);
+		}
 	}
 }
