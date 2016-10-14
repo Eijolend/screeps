b5b8c68a3f9629c865ed5b7c4b40050487b67f4b
Eijolend <j.wallnoefer@hotmail.com>
2016-10-14 23:50:44 +0200

    minor syntax

--- role.dismantler.js
+++ role.dismantler.js
@@ -7,8 +7,8 @@ module.exports ={
     run : function(creep){
         var dismantleFlags = creep.room.find(FIND_FLAGS,{filter: (f) => /dismantle/.test(f.name)});
         var numFlags = dismantleFlags.length;
-        var target = undefined
-        for(i=1,i<=numFlags,i++){
+        var target = undefined;
+        for(i=1 ; i<=numFlags ; i++){
             var myFlag = 'dismantle' + i;
             var structsAtPlace = Game.flags[myFlag].pos.lookFor(LOOK_STRUCTURES);
             if(structsAtPlace.length > 0){
