84578f6b763c521967b1ce63b8372ce8577cc744
Eijolend <j.wallnoefer@hotmail.com>
2016-10-23 00:35:03 +0200

    raidHealers now ALWAYS heal

--- role.raidHealer.js
+++ role.raidHealer.js
@@ -4,17 +4,17 @@ module.exports = {
         if(myflag == undefined){
             myflag = Game.flags['Raid'];
         }
+        var damaged = _.sortBy(creep.pos.findInRange(FIND_MY_CREEPS,1,{filter: (c) => c.hits < c.hitsMax}),'hits');
+        if(damaged.length){
+            creep.heal(damaged[0]);
+        }
+        else{
+            var damagedAtRange = _.sortBy(creep.pos.findInRange(FIND_MY_CREEPS,3,{filter: (c) => c.hits < c.hitsMax}),'hits');
+            if(damagedAtRange.length){
+                creep.rangedHeal(damagedAtRange[0]);
+            }
+        }
         if(creep.room.name==myflag.pos.roomName){
-            var damaged = _.sortBy(creep.pos.findInRange(FIND_MY_CREEPS,1,{filter: (c) => c.hits < c.hitsMax}),'hits');
-            if(damaged.length){
-                creep.heal(damaged[0]);
-            }
-            else{
-                var damagedAtRange = _.sortBy(creep.pos.findInRange(FIND_MY_CREEPS,3,{filter: (c) => c.hits < c.hitsMax}),'hits');
-                if(damagedAtRange.length){
-                    creep.rangedHeal(damagedAtRange[0]);
-                }
-            }
             var closestRaider = creep.pos.findClosestByRange(FIND_MY_CREEPS,{filter: (c) => c.memory.role == 'raider'});
             if(closestRaider != undefined){
                 creep.moveTo(closestRaider,{reusePath:0});
