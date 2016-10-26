212563fba5fd457a223054149d058f9aac428507
Eijolend <j.wallnoefer@hotmail.com>
2016-10-26 22:00:26 +0200

    slight fixes to terminalManagers

--- role.terminalManager.js
+++ role.terminalManager.js
@@ -6,7 +6,10 @@ module.exports = {
         var mineralType = mineral.mineralType;
         var mineralContainer = mineral.pos.findInRange(FIND_STRUCTURES,1,{filter:(s)=>s.structureType == STRUCTURE_CONTAINER})[0];
         if(!creep.memory.getting && _.sum(creep.carry) == 0){
-            if(mineralContainer != undefined && creep.room.terminal != undefined && mineralContainer.store.mineralType >= creep.carryCapacity){
+            if(creep.ticksToLive < 100){
+    			creep.memory.role = 'recycler';
+    		}
+            else if(mineralContainer != undefined && creep.room.terminal != undefined && mineralContainer.store[mineralType] >= creep.carryCapacity){
                 creep.memory.getting = true;
                 creep.memory.mytask = 'getMineral';
             }
@@ -22,8 +25,12 @@ module.exports = {
             creep.memory.getting = false;
             creep.memory.mytask = 'deliver';
         }
-
-        if(creep.memory.mytask == 'runner'){
+        if(creep.memory.mytask == 'deliver'){
+            if(creep.transfer(creep.room.terminal,_.keys(creep.carry)[0]) == ERR_NOT_IN_RANGE){
+                creep.moveTo(creep.room.terminal);
+            }
+        }
+        else if(creep.memory.mytask == 'runner'){
             roleRunner.run(creep);
         }
         else if(creep.memory.mytask = 'getMineral'){
@@ -34,11 +41,6 @@ module.exports = {
         else if(creep.memory.mytask = 'getEnergy'){
             if(creep.withdraw(creep.room.storage,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
                 creep.moveTo(creep.room.storage);
-            }
-        }
-        else if(creep.memory.mytask == 'deliver'){
-            if(creep.transfer(creep.room.terminal,_.keys(creep.carry)[0]) == ERR_NOT_IN_RANGE){
-                creep.moveTo(creep.room.terminal);
             }
         }
 
