7796d4484a02d81a7491881038bd4dfecfe63318
Julius Wallnöfer <j.wallnoefer@hotmail.com>
2016-10-29 15:54:52 +0200

    limited overmining

--- role.miner.js
+++ role.miner.js
@@ -11,7 +11,12 @@ module.exports = {
 				creep.memory.mine = 0;
 			}
 		}
-		var sources = creep.room.find(FIND_SOURCES)
-		tasks.mine(creep,sources[creep.memory.mine]);
+		var mySource = creep.room.find(FIND_SOURCES)[creep.memory.mine];
+		var myContainer = _.filter(mySource.pos.findInRange(FIND_STRUCTURES,1),(s) => s.structureType == STRUCTURE_CONTAINER)[0];
+		var myDropped = mySource.pos.findInRange(FIND_DROPPED_ENERGY,1)[0];
+		var myEnergy = ( myContainer != undefined ? myContainer.store.energy : 0 ) + ( myDropped != undefined ? myDropped.amount : 0 );
+		if (myEnergy < 3000){ //should limit overmining and triggering invaders too early, but is a bit of a waste to check every tick
+			tasks.mine(creep,sources[creep.memory.mine]);
+		}
 	}
 }
