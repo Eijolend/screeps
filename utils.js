713d8ccd58fa6bdc46322c1eda5a9e5c7c3285bd
Eijolend <j.wallnoefer@hotmail.com>
2016-11-12 21:33:31 +0100

    re-added utils

---
+++ utils.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+    isPosEqual : function(firstPos,secondPos){
+        if(!(firstPos && secondPos)){
+            return false
+        }
+        return firstPos.x === secondPos.x && firstPos.y === secondPos.y && firstPos.roomName === secondPos.roomName;
+    }
+}
