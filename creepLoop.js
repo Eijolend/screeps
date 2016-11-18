d54764c1f574c336bcf8d9e5b099ad1e0ef48629
Eijolend <j.wallnoefer@hotmail.com>
2016-11-18 22:04:47 +0100

    simplified creepLoop

--- creepLoop.js
+++ creepLoop.js
@@ -7,41 +7,17 @@ const roleCivilian = require('creep.roleCivilian');
 
 module.exports = {
     run : function(creep){
-        if(creep.task == undefined || tasks.getTarget(creep.task) == undefined){ //if no task or the target does not exist anymore
-            switch (creep.role){
-                case "miner":
-                    roleMiner.getTask(creep);
-                    break;
-                case "runner":
-                    roleRunner.getTask(creep);
-                    break;
-                case "civilian":
-                    roleCivilian.getTask(creep);
-                    break;
-            }
+        switch (creep.role){
+            case "miner":
+                roleMiner.run(creep);
+                break;
+            case "runner":
+                roleRunner.run(creep);
+                break;
+            case "civilian":
+                roleCivilian.run(creep);
+                break;
         }
-
-        if (creep.task != undefined){
-            switch (creep.task.type){
-                case TASK_BUILD:
-                    tasks.build(creep);
-                    break;
-                case TASK_MINE:
-                    tasks.mine(creep);
-                    break;
-                case TASK_FILL:
-                    tasks.fill(creep);
-                    break;
-                case TASK_PICKUP:
-                    tasks.pickup(creep);
-                    break;
-                case TASK_GET_ENERGY:
-                    tasks.getEnergy(creep);
-                    break;
-                case TASK_UPGRADE:
-                    tasks.upgrade(creep);
-                    break;
-            }
-        }
+        tasks.run(creep);
     }
 }
