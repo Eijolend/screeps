ab29e7a3cc87f1472ebbeb093776d7b062edc3b8
Julius Wallnöfer <j.wallnoefer@hotmail.com>
2016-12-01 12:57:09 +0100

    halfed miner cpu, wtf miners?

--- role.miner.js
+++ role.miner.js
@@ -12,7 +12,7 @@ module.exports = {
 			}
 		}
 		var mySource = creep.room.find(FIND_SOURCES)[creep.memory.mine];
-		var myContainer = _.filter(mySource.pos.findInRange(FIND_STRUCTURES,1),(s) => s.structureType == STRUCTURE_CONTAINER)[0];
+		var myContainer = mySource.pos.findInRange(FIND_STRUCTURES,1,{filter:(s)=>s.structureType == STRUCTURE_CONTAINER})[0];
 		var myDropped = mySource.pos.findInRange(FIND_DROPPED_ENERGY,1)[0];
 		var myEnergy = ( myContainer != undefined ? myContainer.store.energy : 0 ) + ( myDropped != undefined ? myDropped.amount : 0 );
 		if (myEnergy < 2000){ //should limit overmining and triggering invaders too early, but is a bit of a waste to check every tick
