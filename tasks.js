21b731212ae313ba8005d19f4f5cfd45f63862aa
Eijolend <j.wallnoefer@hotmail.com>
2017-08-04 12:21:37 +0200

    bugfix

--- tasks.js
+++ tasks.js
@@ -162,15 +162,12 @@ var guard = function(creep){ //like raid, but with a maximal engagement range
 }
 
 var hunt = function(creep){
-	module.exports ={
-	run: function(creep){
-		var target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS,{filter: (c) => !_.contains(playerWhiteList,c.owner.username)});
-		if (creep.attack(target) == ERR_NOT_IN_RANGE){
-			creep.moveTo(target);
-		}
-		if(target === null){
-			creep.task = undefined;
-		}
+	var target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS,{filter: (c) => !_.contains(playerWhiteList,c.owner.username)});
+	if (creep.attack(target) == ERR_NOT_IN_RANGE){
+		creep.moveTo(target);
+	}
+	if(target === null){
+		creep.task = undefined;
 	}
 }
 
