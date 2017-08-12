45380bf1aaf8896b91681484536f5a5f24b17a35
Eijolend <j.wallnoefer@hotmail.com>
2017-08-12 10:27:33 +0200

    integrated profiler

--- main.js
+++ main.js
@@ -9,43 +9,52 @@ require("setupGlobal")(); //global constants and modified prototypes
 
 global.playerWhiteList = [];
 
+const profiler = require("screeps-profiler");
+profiler.registerObject(creepLoop, "creepLoop");
+profiler.registerObject(roomManager, "roomManager");
+profiler.registerObject(defenseManager,"defenseManager");
+profiler.registerObject(markteManager,"marketManager");
+
+profiler.enable();
 module.exports.loop = function(){
-	if(Game.time % 500 == 0){ //garbage collect
-		for(var i in Memory.creeps) {
-        	if(!Game.creeps[i]) {
-            	delete Memory.creeps[i];
+	profiler.wrap(function() {
+		if(Game.time % 500 == 0){ //garbage collect
+			for(var i in Memory.creeps) {
+	        	if(!Game.creeps[i]) {
+	            	delete Memory.creeps[i];
+				}
+			}
+			for(var i in Memory.flags) {
+				if(!Game.flags[i]) {
+		        	delete Memory.flags[i];
+				}
 			}
 		}
-		for(var i in Memory.flags) {
-			if(!Game.flags[i]) {
-	        	delete Memory.flags[i];
-			}
+
+		for(var roomName in Game.rooms){
+			defenseManager.run(Game.rooms[roomName]);
 		}
-	}
+		var myrooms = _.filter(Game.rooms, (r) => r.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_SPAWN}).length > 0 || r.memory.remoteRoom == true);
+		for(var room of myrooms){
+			roomManager.run(room);
+		}
 
-	for(var roomName in Game.rooms){
-		defenseManager.run(Game.rooms[roomName]);
-	}
-	var myrooms = _.filter(Game.rooms, (r) => r.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_SPAWN}).length > 0 || r.memory.remoteRoom == true);
-	for(var room of myrooms){
-		roomManager.run(room);
-	}
+		for(var name in Game.creeps){
+	  		var creep = Game.creeps[name];
+	  		creepLoop.run(creep);
+		}
 
-	for(var name in Game.creeps){
-  		var creep = Game.creeps[name];
-  		creepLoop.run(creep);
-	}
+		if(Game.time % 50 == 0){
+			marketManager.run();
+		}
 
-	if(Game.time % 50 == 0){
-		marketManager.run();
-	}
+		if(Game.spawns.Spawn1.hits < Game.spawns.Spawn1.hitsMax){
+			startRoom.controller.activateSafeMode();
+		}
 
-	if(Game.spawns.Spawn1.hits < Game.spawns.Spawn1.hitsMax){
-		startRoom.controller.activateSafeMode();
-	}
-
-	// if(Game.time % 1000 == 0){
-	// 	Game.spawns.Spawn1.room.requestCreep([TOUGH,TOUGH,ATTACK,ATTACK,ATTACK,MOVE,MOVE,MOVE,MOVE,MOVE],undefined,{role:ROLE_RAIDER , myflag:"blockade", raidMode:"guard"});
-	// 	Game.spawns.Spawn1.room.requestCreep([TOUGH,TOUGH,ATTACK,ATTACK,ATTACK,MOVE,MOVE,MOVE,MOVE,MOVE],undefined,{role:ROLE_RAIDER , myflag:"blockade", raidMode:"guard"});
-	// }
+		// if(Game.time % 1000 == 0){
+		// 	Game.spawns.Spawn1.room.requestCreep([TOUGH,TOUGH,ATTACK,ATTACK,ATTACK,MOVE,MOVE,MOVE,MOVE,MOVE],undefined,{role:ROLE_RAIDER , myflag:"blockade", raidMode:"guard"});
+		// 	Game.spawns.Spawn1.room.requestCreep([TOUGH,TOUGH,ATTACK,ATTACK,ATTACK,MOVE,MOVE,MOVE,MOVE,MOVE],undefined,{role:ROLE_RAIDER , myflag:"blockade", raidMode:"guard"});
+		// }
+	});
 }
